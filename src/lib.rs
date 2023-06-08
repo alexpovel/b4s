a6dc4bd759d40b2b377100955effa19e40d75595
Alex Povel <git@alexpovel.de>
2023-06-08 14:09:45 +0200

    WIP: Use `ascii` crate

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 //! Binary Search Single Sorted String: Perform binary search on a single, delimited
 //! string slice of sorted but unevenly sized substrings.
 
+use ascii::AsciiChar;
 use itertools::Itertools;
 use std::{cmp::Ordering, error::Error, fmt::Display};
 
@@ -35,10 +36,56 @@ use std::{cmp::Ordering, error::Error, fmt::Display};
 ///
 /// 1. [`SortedString::new_checked()`]: recommended, ensuring soundness
 /// 2. [`SortedString::new_unchecked()`]: unsafe, faster
+///
+/// ## Note on ASCII requirement
+///
+/// On instance creation, the separator has to be [within the ASCII
+/// range](https://doc.rust-lang.org/std/primitive.char.html#method.is_ascii). In other
+/// words, **multi-byte (in the sense of UTF-8) separators are not supported**. This
+/// leaves common separators like `\n`, `\t`, `-`, `,` and [many more][ascii::AsciiChar]
+/// available, while arbitrary [`char`]s are not allowed:
+///
+/// ```compile_fail
+/// use b4s::SortedString;
+///
+/// let sep = '🦀';
+/// let haystack = "a,b,c";
+/// // Not allowed:
+/// let _ = SortedString::new_checked(haystack, sep);
+/// ```
+///
+/// UTF-8 being a variable-length encoding, allowing *any* separator [`char`] would
+/// require an initial [linear scan of the input
+/// string](https://doc.rust-lang.org/std/primitive.str.html#method.char_indices):
+///
+/// - either at instance creation time, then saving the results,
+/// - or before each search.
+///
+/// The former takes up a lot of space ([`usize`] pointers, which on 64-bit are much
+/// larger than the separator itself), the latter time. Both are undesirable.
+///
+/// As UTF-8 is fully ASCII-compatible, allowing only that means the string can be
+/// efficiently scanned as bytes. The needle and haystack can still be any UTF-8 string.
+/// No multi-byte code point (with its continuation bytes) [contains bytes in the ASCII
+/// range](https://en.wikipedia.org/wiki/UTF-8#Encoding). Scanning for such an ASCII
+/// separator is therefore sound:
+///
+/// - it won't be found at [non-char
+///   boundaries](https://doc.rust-lang.org/std/primitive.str.html#method.is_char_boundary),
+/// - and it itself will be a boundary.
+///
+/// These properties simplify scanning and allow it to be fast at both instance creation
+/// and search time. The downside is hopefully negligible and in the common case
+/// unnoticeable. Please contact the author if your use case requires multi-byte
+/// support.
+///
+/// The ASCII requirement is manifested in the [type
+/// system](https://dusted.codes/the-type-system-is-a-programmers-best-friend),
+/// enforcing correct usage [at compile time](https://cliffle.com/blog/rust-typestate/).
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SortedString<'a> {
     string: &'a str,
-    sep: char,
+    sep: AsciiChar,
 }
 
 /// The result of a [`SortedString::binary_search()`].
@@ -90,9 +137,10 @@ impl<'a> SortedString<'a> {
     /// # Example
     ///
     /// ```
+    /// use ascii::AsciiChar;
     /// use b4s::SortedString;
     ///
-    /// let sep = ',';
+    /// let sep = AsciiChar::Comma;
     /// let haystack = "a,b,c";
     /// let sorted_string = SortedString::new_checked(haystack, sep);
     ///
@@ -103,57 +151,15 @@ impl<'a> SortedString<'a> {
     ///
     /// This method returns a [`SortedStringCreationError`] in the cases outlined below.
     ///
-    /// ## Example: Creation Fails (Separator Not ASCII)
-    ///
-    /// This error case is owed to an implementation detail. While the separator is a
-    /// [`char`], it has to be [within the ASCII
-    /// range](https://doc.rust-lang.org/std/primitive.char.html#method.is_ascii). In
-    /// other words, **multi-byte (in the sense of UTF-8) separators are not
-    /// supported**. This leaves common separators like `\n`, `\t`, `-`, `,` and many
-    /// more available.
-    ///
-    /// ```
-    /// use b4s::SortedString;
-    ///
-    /// let sep = '🦀';
-    /// let haystack = "a,b,c";
-    /// let sorted_string = SortedString::new_checked(haystack, sep);
-    ///
-    /// assert_eq!(sorted_string, Err(b4s::SortedStringCreationError::NonAsciiSeparator));
-    /// ```
-    ///
-    /// UTF-8 being a variable-length encoding, allowing *any* separator [`char`] would
-    /// require an initial [linear scan of the input
-    /// string](https://doc.rust-lang.org/std/primitive.str.html#method.char_indices):
-    ///
-    /// - either at instance creation time, then saving the results,
-    /// - or before each search.
-    ///
-    /// The former takes up a lot of space ([`usize`] pointers, which on 64-bit are much
-    /// larger than the separator itself), the latter time. Both are undesirable.
-    ///
-    /// As UTF-8 is fully ASCII-compatible, allowing only that means the string can be
-    /// efficiently scanned as bytes. The needle and haystack can still be any UTF-8
-    /// string. No multi-byte code point (with its continuation bytes) [contains bytes
-    /// in the ASCII range](https://en.wikipedia.org/wiki/UTF-8#Encoding). Scanning for
-    /// such an ASCII separator is therefore sound:
-    ///
-    /// - it won't be found at [non-char
-    ///   boundaries](https://doc.rust-lang.org/std/primitive.str.html#method.is_char_boundary),
-    /// - and it itself will be a boundary.
-    ///
-    /// These properties simplify scanning and allow it to be fast at both instance
-    /// creation and search time. The downside is hopefully negligible and in the common
-    /// case unnoticeable.
-    ///
     /// ## Example: Creation Fails (Haystack Not Sorted)
     ///
     /// Binary search requires prior sorting, so creation has to fail.
     ///
     /// ```
+    /// use ascii::AsciiChar;
     /// use b4s::SortedString;
     ///
-    /// let sep = ',';
+    /// let sep = AsciiChar::Comma;
     /// let unsorted_haystack = "a,c,b";
     /// let sorted_string = SortedString::new_checked(unsorted_haystack, sep);
     ///
@@ -165,21 +171,21 @@ impl<'a> SortedString<'a> {
     /// Supplying an empty haystack [`str`] is likely an error and therefore fails.
     ///
     /// ```
+    /// use ascii::AsciiChar;
     /// use b4s::SortedString;
     ///
-    /// let sep = ',';
+    /// let sep = AsciiChar::Comma;
     /// let haystack = "";
     /// let sorted_string = SortedString::new_checked(haystack, sep);
     ///
     /// assert_eq!(sorted_string, Err(b4s::SortedStringCreationError::EmptyHaystack));
     /// ```
-    pub fn new_checked(haystack: &'a str, sep: char) -> Result<Self, SortedStringCreationError> {
+    pub fn new_checked(
+        haystack: &'a str,
+        sep: AsciiChar,
+    ) -> Result<Self, SortedStringCreationError> {
         if haystack.is_empty() {
             return Err(SortedStringCreationError::EmptyHaystack);
-        }
-
-        if !sep.is_ascii() {
-            return Err(SortedStringCreationError::NonAsciiSeparator);
         }
 
         let sorted_string = Self::new(haystack, sep);
@@ -228,7 +234,7 @@ impl<'a> SortedString<'a> {
 
         let haystack = haystack.as_bytes();
 
-        let pred = |c: &&u8| **c == self.sep as u8;
+        let pred = |c: &&u8| **c == self.sep.as_byte();
 
         while low < high {
             let mid = low + (high - low) / 2;
@@ -265,9 +271,10 @@ impl<'a> SortedString<'a> {
     /// # Example: Simple Use
     ///
     /// ```
+    /// use ascii::AsciiChar;
     /// use b4s::SortedString;
     ///
-    /// let sep = ',';
+    /// let sep = AsciiChar::Comma;
     /// let haystack = "a,b,c";
     /// let sorted_string = SortedString::new_unchecked(haystack, sep);
     /// ```
@@ -275,10 +282,11 @@ impl<'a> SortedString<'a> {
     /// # Example: Incorrect Use
     ///
     /// ```
+    /// use ascii::AsciiChar;
     /// use b4s::{SortedString, Span};
     /// use b4s::SearchError::NotFound;
     ///
-    /// let sep = ',';
+    /// let sep = AsciiChar::Comma;
     /// let unsorted_haystack = "a,c,b";
     /// let sorted_string = SortedString::new_unchecked(unsorted_haystack, sep);
     ///
@@ -286,7 +294,7 @@ impl<'a> SortedString<'a> {
     /// assert_eq!(sorted_string.binary_search("b"), Err(NotFound(Span(0, 1))));
     /// ```
     #[must_use]
-    pub fn new_unchecked(string: &'a str, sep: char) -> Self {
+    pub fn new_unchecked(string: &'a str, sep: AsciiChar) -> Self {
         Self::new(string, sep)
     }
 
@@ -300,9 +308,10 @@ impl<'a> SortedString<'a> {
     /// # Example
     ///
     /// ```
+    /// use ascii::AsciiChar;
     /// use b4s::{SortedString, Span};
     ///
-    /// let sep = '\n';
+    /// let sep = AsciiChar::LineFeed;
     /// // Perhaps this was read directly from a file, where sorting is unreliable/unknown.
     /// let unsorted_haystack = "c\nb\na";
     /// let sorted_haystack = SortedString::sort(unsorted_haystack, sep);
@@ -314,21 +323,21 @@ impl<'a> SortedString<'a> {
     /// ```
     ///
     #[must_use]
-    pub fn sort(string: &str, sep: char) -> String {
+    pub fn sort(string: &str, sep: AsciiChar) -> String {
         string
-            .split(sep)
+            .split(sep.as_char())
             .sorted()
             .collect::<Vec<&str>>()
             .join(&sep.to_string())
     }
 
-    fn new(string: &'a str, sep: char) -> Self {
+    fn new(string: &'a str, sep: AsciiChar) -> Self {
         Self { string, sep }
     }
 
     fn is_sorted(&self) -> bool {
         self.string
-            .split(self.sep)
+            .split(self.sep.as_char())
             .tuple_windows()
             .all(|(a, b)| a <= b)
     }
@@ -341,8 +350,6 @@ pub enum SortedStringCreationError {
     NotSorted,
     /// The passed haystack was empty.
     EmptyHaystack,
-    /// The passed separator was not within the ASCII range.
-    NonAsciiSeparator,
 }
 
 impl Error for SortedStringCreationError {}
@@ -352,7 +359,6 @@ impl Display for SortedStringCreationError {
         match self {
             Self::NotSorted => write!(f, "The provided string is not sorted."),
             Self::EmptyHaystack => write!(f, "The provided string is empty."),
-            Self::NonAsciiSeparator => write!(f, "The provided separator is not ASCII."),
         }
     }
 }
--- tests/main.rs
+++ tests/main.rs
@@ -1,156 +1,272 @@
+use ascii::AsciiChar;
 use b4s::{SearchError, SearchResult, SortedString, Span};
 use rstest::rstest;
 
-fn base_test(needle: &str, haystack: &str, sep: char, expected: SearchResult) {
+fn base_test(needle: &str, haystack: &str, sep: AsciiChar, expected: SearchResult) {
     let ss = SortedString::new_checked(haystack, sep).unwrap();
     assert_eq!(ss.binary_search(needle), expected);
 }
 
 #[rstest]
-#[case("abc", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(0, 3)))]
-#[case("def", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(4, 7)))]
-#[case("ghi", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(8, 11)))]
-#[case("jkl", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(12, 15)))]
-#[case("mno", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(16, 19)))]
-#[case("pqr", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(20, 23)))]
-#[case("stu", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(24, 27)))]
-#[case("vwx", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(28, 31)))]
-#[case("yz", "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz", ',', Ok(Span(32, 34)))]
+#[case(
+    "abc",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(0, 3))
+)]
+#[case(
+    "def",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(4, 7))
+)]
+#[case(
+    "ghi",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(8, 11))
+)]
+#[case(
+    "jkl",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(12, 15))
+)]
+#[case(
+    "mno",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(16, 19))
+)]
+#[case(
+    "pqr",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(20, 23))
+)]
+#[case(
+    "stu",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(24, 27))
+)]
+#[case(
+    "vwx",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(28, 31))
+)]
+#[case(
+    "yz",
+    "abc,def,ghi,jkl,mno,pqr,stu,vwx,yz",
+    AsciiChar::Comma,
+    Ok(Span(32, 34))
+)]
 fn test_base_cases(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("mn", "abc,mno,yz", ',', Err(SearchError::NotFound(Span(0, 3))))]
-#[case("a", "abc,def,yz", ',', Err(SearchError::NotFound(Span(0, 3))))]
-#[case("z", "abc,def,yz", ',', Err(SearchError::NotFound(Span(8, 10))))]
+#[case(
+    "mn",
+    "abc,mno,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(0, 3)))
+)]
+#[case(
+    "a",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(0, 3)))
+)]
+#[case(
+    "z",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(8, 10)))
+)]
 fn test_needle_shorter_than_any_haystack_item(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("abcd", "abc,def,yz", ',', Err(SearchError::NotFound(Span(0, 3))))]
-#[case("xyz", "abc,def,yz", ',', Err(SearchError::NotFound(Span(4, 7))))]
-#[case("zyz", "abc,def,yz", ',', Err(SearchError::NotFound(Span(8, 10))))]
+#[case(
+    "abcd",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(0, 3)))
+)]
+#[case(
+    "xyz",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(4, 7)))
+)]
+#[case(
+    "zyz",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(8, 10)))
+)]
 fn test_needle_longer_than_any_haystack_item(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("abc", "a,b,c", ',', Err(SearchError::NotFound(Span(0, 1))))]
+#[case(
+    "abc",
+    "a,b,c",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(0, 1)))
+)]
 fn test_single_character_haystack(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("a", "a,def,yz", ',', Ok(Span(0, 1)))]
-#[case("a", "abc,def,yz", ',', Err(SearchError::NotFound(Span(0, 3))))]
-#[case("z", "abc,def,z", ',', Ok(Span(8, 9)))]
-#[case("z", "abc,def,yz", ',', Err(SearchError::NotFound(Span(8, 10))))]
+#[case("a", "a,def,yz", AsciiChar::Comma, Ok(Span(0, 1)))]
+#[case(
+    "a",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(0, 3)))
+)]
+#[case("z", "abc,def,z", AsciiChar::Comma, Ok(Span(8, 9)))]
+#[case(
+    "z",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(8, 10)))
+)]
 fn test_single_character_needle(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("a", "a,b,c", ',', Ok(Span(0, 1)))]
-#[case("c", "a,b,c", ',', Ok(Span(4, 5)))]
-#[case("d", "a,b,c", ',', Err(SearchError::NotFound(Span(4, 5))))]
+#[case("a", "a,b,c", AsciiChar::Comma, Ok(Span(0, 1)))]
+#[case("c", "a,b,c", AsciiChar::Comma, Ok(Span(4, 5)))]
+#[case("d", "a,b,c", AsciiChar::Comma, Err(SearchError::NotFound(Span(4, 5))))]
 fn test_single_character_needle_and_haystack(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("aaa", "aaa,def,yz", ',', Ok(Span(0, 3)))]
-#[case("aaa", "abc,def,yz", ',', Err(SearchError::NotFound(Span(0, 3))))]
-#[case("zzz", "abc,def,zzz", ',', Ok(Span(8, 11)))]
-#[case("zzz", "abc,def,yz", ',', Err(SearchError::NotFound(Span(8, 10))))]
+#[case("aaa", "aaa,def,yz", AsciiChar::Comma, Ok(Span(0, 3)))]
+#[case(
+    "aaa",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(0, 3)))
+)]
+#[case("zzz", "abc,def,zzz", AsciiChar::Comma, Ok(Span(8, 11)))]
+#[case(
+    "zzz",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(8, 10)))
+)]
 fn test_repeated_character_needle(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("", ",", ',', Ok(Span(0, 0)))]
-#[case("", ",,", ',', Ok(Span(1, 1)))]
-#[case("", "abc", ',', Err(SearchError::NotFound(Span(0, 3))))]
+#[case("", ",", AsciiChar::Comma, Ok(Span(0, 0)))]
+#[case("", ",,", AsciiChar::Comma, Ok(Span(1, 1)))]
+#[case("", "abc", AsciiChar::Comma, Err(SearchError::NotFound(Span(0, 3))))]
 fn test_empty_needle(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("abc", "abc", ',', Ok(Span(0, 3)))]
-#[case("abc", "abc,def", ',', Ok(Span(0, 3)))]
-#[case("abc", ",", ',', Err(SearchError::NotFound(Span(0, 0))))]
-#[case("", ",,,,", ',', Ok(Span(2, 2)))]
-#[case("abc", ",,,abc", ',', Ok(Span(3, 6)))]
+#[case("abc", "abc", AsciiChar::Comma, Ok(Span(0, 3)))]
+#[case("abc", "abc,def", AsciiChar::Comma, Ok(Span(0, 3)))]
+#[case("abc", ",", AsciiChar::Comma, Err(SearchError::NotFound(Span(0, 0))))]
+#[case("", ",,,,", AsciiChar::Comma, Ok(Span(2, 2)))]
+#[case("abc", ",,,abc", AsciiChar::Comma, Ok(Span(3, 6)))]
 fn test_oddly_shaped_haystack(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("nmo", "abc,mno,yz", ',', Err(SearchError::NotFound(Span(4, 7))))]
-#[case("cba", "abc,def,yz", ',', Err(SearchError::NotFound(Span(0, 3))))]
+#[case(
+    "nmo",
+    "abc,mno,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(4, 7)))
+)]
+#[case(
+    "cba",
+    "abc,def,yz",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(0, 3)))
+)]
 fn test_switched_characters_needle(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("abc", "abc-def-yz", '-', Ok(Span(0, 3)))]
-#[case("abc", "abc\0def\0yz", '\0', Ok(Span(0, 3)))]
-#[case("defg", "abc\0def\0yz", '\0', Err(SearchError::NotFound(Span(4, 7))))]
+#[case("abc", "abc-def-yz", AsciiChar::Minus, Ok(Span(0, 3)))]
+#[case("abc", "abc\0def\0yz", AsciiChar::Null, Ok(Span(0, 3)))]
+#[case(
+    "defg",
+    "abc\0def\0yz",
+    AsciiChar::Null,
+    Err(SearchError::NotFound(Span(4, 7)))
+)]
 fn test_different_separators(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
@@ -160,22 +276,22 @@ fn test_different_separators(
 #[case(
     "abc",
     "Hund\nKatze\nMaus",
-    '\n',
+    AsciiChar::LineFeed,
     Err(SearchError::NotFound(Span(11, 15)))
 )]
 #[case(
     "ABC",
     "Hund\nKatze\nMaus",
-    '\n',
+    AsciiChar::LineFeed,
     Err(SearchError::NotFound(Span(0, 4)))
 )]
-#[case("Hund", "Hund\nKatze\nMaus", '\n', Ok(Span(0, 4)))]
-#[case("Katze", "Hund\nKatze\nMaus", '\n', Ok(Span(5, 10)))]
-#[case("Maus", "Hund\nKatze\nMaus", '\n', Ok(Span(11, 15)))]
+#[case("Hund", "Hund\nKatze\nMaus", AsciiChar::LineFeed, Ok(Span(0, 4)))]
+#[case("Katze", "Hund\nKatze\nMaus", AsciiChar::LineFeed, Ok(Span(5, 10)))]
+#[case("Maus", "Hund\nKatze\nMaus", AsciiChar::LineFeed, Ok(Span(11, 15)))]
 fn test_real_world_examples(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
@@ -185,15 +301,20 @@ fn test_real_world_examples(
 #[case(
     "Hündin",
     "Hund\nKatze\nMaus",
-    '\n',
+    AsciiChar::LineFeed,
     Err(SearchError::NotFound(Span(5, 10)))
 )]
-#[case("Hündin", "Hündin\nKatze\nMaus", '\n', Ok(Span(0, 7)))]
-#[case("Mäuschen", "Hündin\nKatze\nMäuschen", '\n', Ok(Span(14, 23)))]
+#[case("Hündin", "Hündin\nKatze\nMaus", AsciiChar::LineFeed, Ok(Span(0, 7)))]
+#[case(
+    "Mäuschen",
+    "Hündin\nKatze\nMäuschen",
+    AsciiChar::LineFeed,
+    Ok(Span(14, 23))
+)]
 fn test_real_world_examples_with_multibyte_characters(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
@@ -203,60 +324,69 @@ fn test_real_world_examples_with_multibyte_characters(
 #[case(
     "Abdämpfung",
     "Abdämpfung\nAbenteuer\nAbschluss",
-    '\n',
+    AsciiChar::LineFeed,
     Ok(Span(0, 11))
 )]
-#[case("Abdämpfung", "Abdrehen\nAbdämpfung\nAbschluss", '\n', Ok(Span(9, 20)))]
+#[case(
+    "Abdämpfung",
+    "Abdrehen\nAbdämpfung\nAbschluss",
+    AsciiChar::LineFeed,
+    Ok(Span(9, 20))
+)]
 fn test_real_world_examples_with_common_prefixes(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("abc,def", "abc,def,ghi", ',', Err(SearchError::NotFound(Span(0, 3))))]
+#[case(
+    "abc,def",
+    "abc,def,ghi",
+    AsciiChar::Comma,
+    Err(SearchError::NotFound(Span(0, 3)))
+)]
 fn test_needle_contains_separator(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
+    #[case] sep: AsciiChar,
     #[case] expected: SearchResult,
 ) {
     base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("", ",a", ',', true)]
+#[case("", ",a", AsciiChar::Comma, Ok(Span(0, 0)))]
 #[should_panic]
 fn test_buggy_cases_to_be_fixed(
     #[case] needle: &str,
     #[case] haystack: &str,
-    #[case] sep: char,
-    #[case] expected: bool,
+    #[case] sep: AsciiChar,
+    #[case] expected: SearchResult,
 ) {
-    let ss = SortedString::new_checked(haystack, sep).unwrap();
-    assert_eq!(ss.binary_search(needle).is_ok(), expected);
+    base_test(needle, haystack, sep, expected)
 }
 
 #[rstest]
-#[case("c,b,a", ',')]
-#[case("a,", ',')]
-#[case("a,b,c,", ',')]
-#[case("a,,", ',')]
-#[case(",a,", ',')]
-#[case("Hello,Cruel,World", ',')]
-fn test_unsorted_haystack(#[case] haystack: &str, #[case] sep: char) {
+#[case("c,b,a", AsciiChar::Comma)]
+#[case("a,", AsciiChar::Comma)]
+#[case("a,b,c,", AsciiChar::Comma)]
+#[case("a,,", AsciiChar::Comma)]
+#[case(",a,", AsciiChar::Comma)]
+#[case("Hello,Cruel,World", AsciiChar::Comma)]
+fn test_unsorted_haystack(#[case] haystack: &str, #[case] sep: AsciiChar) {
     let ss = SortedString::new_checked(haystack, sep);
     assert_eq!(ss, Err(b4s::SortedStringCreationError::NotSorted));
 }
 
 #[rstest]
-#[case("", ',')]
-#[case("", '-')]
-#[case("", '\0')]
-fn test_empty_haystack(#[case] haystack: &str, #[case] sep: char) {
+#[case("", AsciiChar::Comma)]
+#[case("", AsciiChar::Minus)]
+#[case("", AsciiChar::Null)]
+fn test_empty_haystack(#[case] haystack: &str, #[case] sep: AsciiChar) {
     let ss = SortedString::new_checked(haystack, sep);
     assert_eq!(ss, Err(b4s::SortedStringCreationError::EmptyHaystack));
 }
