64825761511209b0e7986714c53fa1b393308c9f
Alex Povel <git@alexpovel.de>
2023-06-09 15:23:31 +0200

    WIP: Add fuzzing

--- Justfile
+++ Justfile
@@ -10,3 +10,21 @@ set ignore-comments := true
 onboard:
     pip install pre-commit && pre-commit install --hook-type pre-push --hook-type pre-commit --hook-type commit-msg
     cargo install cargo-tarpaulin
+
+# Runs fuzz testing.
+[unix]
+fuzz: install-afl
+    cd {{ justfile_directory() / 'fuzz' }} && cargo afl build
+
+    # Kill any running fuzzers, they like to get stuck.
+    kill -9 $(lsof -t {{ justfile_directory() / 'fuzz' / 'out' }}/*) || true
+
+    cd {{ justfile_directory() / 'fuzz' }} && cargo afl fuzz -i in -o out target/debug/afl-target
+
+# Provisions AFL (https://rust-fuzz.github.io/book/afl/setup.html#tools).
+[unix]
+install-afl:
+    command -v gcc > /dev/null || { sudo apt-get update && sudo apt-get install --yes gcc; }
+    command -v make > /dev/null || { sudo apt-get update && sudo apt-get install --yes make; }
+    command -v lsof > /dev/null || { sudo apt-get update && sudo apt-get install --yes lsof; }
+    command -v cargo-afl > /dev/null || { cargo install afl; }
---
+++ fuzz/src/main.rs
@@ -0,0 +1,80 @@
+use afl::fuzz;
+use arbitrary::{Arbitrary, Unstructured};
+use b4s::AsciiChar;
+use std::{fmt::Display, fs::OpenOptions, io::Write};
+
+/// Input signature for fuzzing.
+///
+/// Seems very inefficient, but there's limited capabilities to prepare/structure input.
+/// See https://rust-fuzz.github.io/book/afl/tutorial.html. Note also:
+///
+/// > For performance reasons, it is recommended that you use the native type &[u8] when
+/// > possible.
+///
+/// (Source: https://docs.rs/afl/0.13.0/afl/macro.fuzz.html)
+///
+/// I decided against using raw bytes, instead opting for structure via `Arbitrary`.
+/// More maintainable, readable etc.
+///
+/// Also of interest might be what Google has to say on the topic at
+/// https://github.com/google/fuzzing/blob/41d7725e6a12ec2e0a89b68fafacb4bb5af8fac7/docs/split-inputs.md#examples
+/// . Note the example with regex, which matches the use case here.
+#[derive(Arbitrary, Debug)]
+struct Input {
+    #[arbitrary(with = unstructured_to_ascii_char)]
+    separator: AsciiChar,
+    needle: String,
+    haystack: String,
+}
+
+impl Display for Input {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let sep = AsciiChar::from_ascii(self.separator);
+        write!(
+            f,
+            "separator: {:?}, needle: {:?}, haystack: {:?}",
+            sep, self.needle, self.haystack
+        )
+    }
+}
+
+fn unstructured_to_ascii_char(u: &mut Unstructured) -> arbitrary::Result<AsciiChar> {
+    if let Ok(b) = u.arbitrary::<u8>() {
+        if let Ok(char) = AsciiChar::from_ascii(b) {
+            return Ok(char);
+        }
+    }
+
+    Err(arbitrary::Error::IncorrectFormat)
+}
+
+fn main() {
+    let filename = "fuzz-inputs.txt";
+
+    let mut data_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(filename)
+        .expect("Cannot input data file.");
+
+    fuzz!(|data: Input| {
+        // Chances of us passing `new_checked` are exceedingly slim, so go unchecked.
+        // Don't care for matches here anyway, just want to see if we panic.
+        let ss = b4s::SortedString::new_unchecked(&data.haystack, data.separator);
+
+        let res = ss.binary_search(&data.needle);
+
+        if let Err(b4s::SearchError::EncodingError) = res {
+            // https://github.com/rust-fuzz/afl.rs/issues/215#issuecomment-1039707502
+            println!("{data}");
+            panic!("Got encoding error");
+        }
+
+        // It's not easy to get insight into the input data unless a panic occurred, so
+        // write it out. This costs c. 20% in throughput performance. Comment out once
+        // you're convinced results are good.
+        data_file
+            .write_all(format!("{data}\n").as_bytes())
+            .expect("Cannot write to data file.");
+    });
+}
